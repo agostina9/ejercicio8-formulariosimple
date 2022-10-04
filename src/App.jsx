b77af07d407079ea4c5634a4292f296f4d485c04
Agostina <lozagostina@gmail.com>
2022-10-04 11:53:10 -0300

    agrego estructura inicial y react bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+      <h1 className="display-4">prueba</h1>
+     </Container>
+    
+  );
+}
+
+export default App;
